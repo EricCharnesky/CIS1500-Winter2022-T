ceecbc6f9c55b007befeb784849b707f1a1d7ee7
Eric Charnesky <eric.charnesky@careevolution.com>
2022-01-11 20:02:31 -0500

    Hello World Introduction

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Eric Charnesky (he/him)");
+        System.out.println("Doctor of Engineering in Automotive Systems and Moblitiy");
+        System.out.println("15ish years programming experience, c++, java, c#, python, PowerShell, VB, ASP.net, PHP");
+        System.out.println("I'm taking the class because I'm the teacher");
+        System.out.println("I play D&D with my kids and friends, I have 6 kids");
+    }
+
+}
