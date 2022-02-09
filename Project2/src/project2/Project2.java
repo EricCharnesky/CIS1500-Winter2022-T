a9de9489ebf384c59fe91af74be4dd2799dd6857
Eric Charnesky <eric.charnesky@careevolution.com>
2022-02-08 19:31:51 -0500

    forgot to save

--- Project2/src/project2/Project2.java
+++ Project2/src/project2/Project2.java
@@ -1,40 +1,46 @@
-
 package project2;
 
 public class Project2 {
 
+    // class level variable, accessible EVERYWHERE in the class
+    static int energy = 10;
+
     public static void main(String[] args) {
         displayIntroStory();
-        
+
         String action = askTheUserWhatTheyWantToDo();
         int numberOfTurns = 1;
-        
-        while ( !action.equalsIgnoreCase("someescapeword")){
-            if ( action.equalsIgnoreCase("help")){
-                displayHelp();
-            } else if ( action.equalsIgnoreCase("sleep")){
-                // TODO
-            } else{
-                System.out.println("You can't do that right now");
-            }
+
+        while (!action.equalsIgnoreCase("someEscapeWord")) {
+            doAction(action);
             action = askTheUserWhatTheyWantToDo();
         }
-        
+
         System.out.println("Congrats you escaped our terrible game!");
-        System.out.println("It took you " + turns + " to get out!");
+        System.out.println("It took you " + numberOfTurns + " to get out!");
     }
-    
-    public static void displayHelp(){
+
+    public static void doAction(String action) {
+        if (action.equalsIgnoreCase("help")) {
+            displayHelp();
+        } else if (action.equalsIgnoreCase("sleep")) {
+            energy = 10;
+        } else {
+            System.out.println("You can't do that right now");
+        }
+    }
+
+    public static void displayHelp() {
         // TODO
     }
-    
+
     public static void displayIntroStory() {
         // TODO
     }
-    
-    public static String askTheUserWhatTheyWantToDo(){
+
+    public static String askTheUserWhatTheyWantToDo() {
         // TODO 
         return "";
     }
-    
+
 }
