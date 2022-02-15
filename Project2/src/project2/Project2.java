4a6911d9fe0390f3e6ff8c0cb69a411b384de831
Eric Charnesky <eric.charnesky@careevolution.com>
2022-02-15 18:39:36 -0500

    project 2

--- Project2/src/project2/Project2.java
+++ Project2/src/project2/Project2.java
@@ -1,17 +1,19 @@
 package project2;
 
+import java.util.Scanner;
+
 public class Project2 {
 
     // class level variable, accessible EVERYWHERE in the class
     static int energy = 10;
+    static int numberOfTurns = 0;
 
     public static void main(String[] args) {
         displayIntroStory();
 
         String action = askTheUserWhatTheyWantToDo();
-        int numberOfTurns = 1;
 
-        while (!action.equalsIgnoreCase("someEscapeWord")) {
+        while (!action.equalsIgnoreCase("get me out of here!")) {
             doAction(action);
             action = askTheUserWhatTheyWantToDo();
         }
@@ -21,26 +23,68 @@ public class Project2 {
     }
 
     public static void doAction(String action) {
+        numberOfTurns++;
         if (action.equalsIgnoreCase("help")) {
             displayHelp();
+            numberOfTurns--;
         } else if (action.equalsIgnoreCase("sleep")) {
             energy = 10;
+            System.out.println("You sleep and wake up feeling energized");
+        } else if (action.equalsIgnoreCase("jump")) {
+            System.out.println("You jump and land on your ankle funny, ow!");
+        } else if (action.equalsIgnoreCase("hide")) {
+            System.out.println("You hide from your shadow!");
+        } else if (action.equalsIgnoreCase("eat")) {
+            System.out.println("You eat some stale biscuts!");
+        } else if (action.equalsIgnoreCase("go north")) {
+            if (hasEnoughEnergy()) {
+                System.out.println("you go north into the forest");
+                energy -= 1;
+            }
+        } else if (action.equalsIgnoreCase("go east")) {
+            if (hasEnoughEnergy()) {
+                System.out.println("you go east into the mountains");
+                energy -= 1;
+            }
+        } else if (action.equalsIgnoreCase("go south")) {
+            if (hasEnoughEnergy()) {
+                System.out.println("you go south into the lake");
+                energy -= 1;
+            }
+        } else if (action.equalsIgnoreCase("go west")) {
+            if (hasEnoughEnergy()) {
+                System.out.println("you go west into the plains");
+                energy -= 1;
+            }
         } else {
             System.out.println("You can't do that right now");
+            numberOfTurns--;
         }
     }
 
     public static void displayHelp() {
-        // TODO
+        System.out.println("You can: go north, go east, go south, "
+                + "go west, jump, sleep, hide, eat");
     }
 
     public static void displayIntroStory() {
-        // TODO
+        System.out.println("Welcome to our terrible game,"
+                + " you wake up in the woods and don't remember anything!");
     }
 
     public static String askTheUserWhatTheyWantToDo() {
-        // TODO 
-        return "";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What do you want to do?");
+        return keyboard.nextLine();
+    }
+
+    public static boolean hasEnoughEnergy() {
+        if (energy > 0) {
+            return true;
+        }
+
+        System.out.println("you are too tired to do that, you need to sleep");
+        return false;
     }
 
 }
