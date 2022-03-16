637888fa3f611621ec8b60a578f2947d722df751
Eric Charnesky <eric.charnesky@careevolution.com>
2022-03-15 20:04:41 -0400

    arrays and arraylists

--- Lab7/src/lab7/Main.java
+++ Lab7/src/lab7/Main.java
@@ -14,5 +14,14 @@ public class Main {
                 "January 1st, 2017", "Murach", "Joel", "22 hours",
                 "2 minutes");
         System.out.println(audioBook.getInfo());
+
+        Book[] shelf = new Book[3];
+        shelf[0] = book;
+        shelf[1] = ebook;
+        shelf[2] = audioBook;
+
+        for ( Book bookOnShelf : shelf){
+            System.out.println(bookOnShelf.getInfo());
+        }
     }
 }
